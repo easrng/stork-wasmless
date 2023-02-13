e1f3a9903bfe5a8f6549f303122029460bc977dc
easrng <me@easrng.net>
2023-02-13 04:47:52 -0500

    switch from Oz to O4

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ cd stork-wasm
 wasm-pack build --target bundler --out-name stork_js -- --no-default-features --features="v3"
 wasm-pack build --target web --out-name stork_wasm -- --no-default-features --features="v3"
 cd ..
-wasm2js stork-wasm/pkg/stork_js_bg.wasm -o stork-wasm/pkg/stork_js_bg.wasm.js -Oz
+wasm2js stork-wasm/pkg/stork_js_bg.wasm -o stork-wasm/pkg/stork_js_bg.wasm.js -O4
 sed -i 's/stork_js_bg.wasm/stork_js_bg.wasm.js/' stork-wasm/pkg/stork_js_bg.js
 sed -i 's/stork_js_bg.wasm/stork_js_bg.wasm.js/' stork-wasm/pkg/stork_js.js
 cd ..
